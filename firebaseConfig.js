3b71423399fefbef3df0e2e11d1036ede9deb8dd
choi <cysong12@gmail.com>
2019-11-28 19:36:02 -0800

    readitem
    readitem

---
+++ firebaseConfig.js
@@ -0,0 +1,21 @@
+src="https://www.gstatic.com/firebasejs/7.4.0/firebase-app.js"
+
+src="https://www.gstatic.com/firebasejs/7.4.0/firebase-analytics.js"
+
+
+src="https://www.gstatic.com/firebasejs/7.4.0/firebase-auth.js"
+src="https://www.gstatic.com/firebasejs/7.4.0/firebase-firestore.js"
+
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyCZJFlf1Q-uKORv49mZwHqKwYxjZMYpYxg",
+    authDomain: "fir-f9d7a.firebaseapp.com",
+    databaseURL: "https://fir-f9d7a.firebaseio.com",
+    projectId: "fir-f9d7a",
+    storageBucket: "fir-f9d7a.appspot.com",
+    messagingSenderId: "526422688230",
+    appId: "1:526422688230:web:a4c71e058f33d2fa30d627"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
