7c42dfe869890b46f5a8197386becdb532cd0683
Dongmin Lee <donglmin@icloud.com>
2019-11-30 18:10:10 -0800

    deleted style.css line from html script

---
+++ friends.js
@@ -0,0 +1,10 @@
+//write to friendRequest document
+requestRef: db.doc("users/"+doc.id)
+
+//delete from friendRequest document
+
+//Write to friendList document
+
+//read from friendRequestList document
+
+//read from friendList document
