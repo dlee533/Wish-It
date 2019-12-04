d467334cc9b08d39ecca4df88a449b97634d5023
Dongmin Lee <donglmin@icloud.com>
2019-12-04 14:25:25 -0800

    signout

--- signout.js
+++ signout.js
@@ -1,9 +1,11 @@
 function signOut(){
     firebase.auth().signOut().then(function(){
         console.log("Sign-out successful");
+        localStorage.clear();
+        window.location.href = "index.html";
     }).catch(function(error){
         console.log("Sign out error!")
-    })
-}
+    });
+};
 
 document.getElementById("signOut").onclick = signOut;
